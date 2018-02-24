285db8f7b578e76b783d7fadd6871846e359f424
michaelalperovich <misha.alperovich1@gmail.com>
2018-02-24 14:39:35 -0500

    First Version

--- app/src/main/java/com/example/michaelalperovich/schooliscool/MainActivity.java
+++ app/src/main/java/com/example/michaelalperovich/schooliscool/MainActivity.java
@@ -2,12 +2,141 @@ package com.example.michaelalperovich.schooliscool;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    TextView promptTextView;
+    TextView statsTextView;
+    Button leftButton;
+    Button rightButton;
+    private int promptNum = 0;
+    private ArrayList<Prompt> prompts = new ArrayList<>();
+    private int stress = 50;
+    private int energy = 50;
+    private int friends = 50;
+    private int grades = 50;
+    private int hour = 6;
+    private int minute = 0;
+    private int choice;
+    private boolean initialised = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        promptTextView = findViewById(R.id.promptTextView);
+        statsTextView = findViewById(R.id.statsTextView);
+        leftButton = findViewById(R.id.leftButton);
+        rightButton = findViewById(R.id.rightButton);
+        main();
+
+    }
+
+
+    private void timeIncrease() {
+        minute += 30;
+        if (minute >= 60) {
+            hour += minute / 60;
+            minute -= 60;
+        }
+    }
+
+    private void printStats() {
+        String tmp = new String();
+        tmp += "Stress: " + stress;
+        tmp += " Energy: " + energy;
+        tmp += " Friends: " + friends;
+        tmp += " Grades: " + grades;
+        statsTextView.setText(tmp);
+    }
+
+    private void displayPrompt() {
+        if(promptNum >= prompts.size()){
+            promptNum = 0;
+        }
+        promptTextView.setText(prompts.get(promptNum).toString());
+        leftButton.setText(prompts.get(promptNum).getOption(0));
+        rightButton.setText(prompts.get(promptNum).getOption(1));
+    }
+
+    private void initializePrompts() {
+        prompts.add(new Prompt("You did your homework until 1 am. When do you wake up?", "6:00 A.M.", "10:00 A.M.", -5, 10, -15, 5, 0, -5, 10, -10));
+        prompts.add(new Prompt("You've been invited to a party! Do you go?", "YES", "NO", 5, 0, 5, -5, 20, -10, -10, 15));
+        prompts.add(new Prompt("Spring Season is starting! Do you sign up for a sport?", "YES", "NO", 10, -5, 10, -5, 10, -5, -5, 15));
+        prompts.add(new Prompt("Your crush tells you that they really like you. Do you ask them out for a date?", "YES", "NO", 15, 0, 10, -5, 5, -5, -10, 15));
+        prompts.add(new Prompt("Your neighbors are going out for the weekend. They ask you if you could babysit their son. Do you?", "YES", "NO", 5, 0, -5, 0, -5, 5, 0, 5));
+        prompts.add(new Prompt("There's a lecture in a university in your area that is open to the public. Do you go?", "YES", "NO", 0, 0, -5, 0, -5, 5, 15, -5));
+        prompts.add(new Prompt("There's a scholarship available for college. Do you try to get it?", "YES", "NO", 15, -5, -15, 5, -5, 5, -5, 5));
+        prompts.add(new Prompt("There's a job opening in your area and you fit the criteria that would be needed to fill it. Do you apply for the job?", "YES", "NO", 15, -5, -10, 10, -10, 10, -5, 15));
+    }
+
+    public void choice(int option){
+
+        Prompt currentPrompt = prompts.get(promptNum);
+        stress += currentPrompt.getStressChange()[option];
+        energy += currentPrompt.getEnergyChange()[option];
+        friends += currentPrompt.getFriendsChange()[option];
+        grades += currentPrompt.getGradesChange()[option];
+
+    }
+
+    public void getButtonPressed(String i){
+        initialised = false;
+        if (i.equals("left")) {
+            choice = 0;
+        }
+        else if (i.equals("right")) {
+            choice = 1;
+        }
+        else {
+            choice = -1;
+        }
+        main2();
+    }
+
+    public void main() {
+
+        initializePrompts();
+        initialised = true;
+        printStats();
+        //System.out.println("Time: " + hour + ": " + (minute / 10) + (minute % 10));
+        displayPrompt();
+    }
+
+    private void main2() {
+        if (choice != -1) {
+            choice(choice);
+            promptNum++;
+            //System.out.println("time passes");
+            printStats();
+            timeIncrease();
+            main();
+        }
+        else {
+            statsTextView.setText("You Failed!");
+        }
+    }
+
+    public Prompt nextPrompt() {
+        //TODO
+        return null;
+    }
+
+
+    public void onYesClick(View View) {
+        if (initialised){
+            getButtonPressed("left");
+        }
+    }
+
+    public void onNoClick(View View) {
+        if (initialised) {
+            getButtonPressed("right");
+        }
     }
 }
---
+++ app/src/main/java/com/example/michaelalperovich/schooliscool/Prompt.java
@@ -0,0 +1,51 @@
+package com.example.michaelalperovich.schooliscool;
+
+public class Prompt {
+
+    private String prompt;
+    private String[] options = new String[2];
+    private int[] stress = new int[2];
+    private int[] energy = new int[2];
+    private int[] friends = new int[2];
+    private int[] grades = new int[2];
+    private double orderNum;
+
+    public Prompt(String prompt, String option1, String option2, int stress1, int stress2, int energy1, int energy2, int friends1, int friends2, int grades1, int grades2) {
+        this.prompt = prompt;
+        this.options[0] = option1;
+        this.options[1] = option2;
+        this.stress[0] = stress1;
+        this.stress[1] = stress2;
+        this.energy[0] = energy1;
+        this.energy[1] = energy2;
+        this.friends[0] = friends1;
+        this.friends[1] = friends2;
+        this.grades[0] = grades1;
+        this.grades[1] = grades2;
+        this.orderNum = Math.random();
+    }
+
+    public String getOption(int option) {
+        return this.options[option];
+    }
+
+    public int[] getStressChange(){
+        return stress;
+    }
+
+    public int[] getEnergyChange(){
+        return energy;
+    }
+
+    public int[] getFriendsChange(){
+        return friends;
+    }
+
+    public int[] getGradesChange(){
+        return grades;
+    }
+
+    public String toString() {
+        return this.prompt;
+    }
+}
