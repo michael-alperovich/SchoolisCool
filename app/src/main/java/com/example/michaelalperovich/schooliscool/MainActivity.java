e4a8cc6a91e73d0b426104b5a1579e707d41a613
michaelalperovich <misha.alperovich1@gmail.com>
2018-02-24 18:05:21 -0500

    bug fixes

--- app/src/main/java/com/example/michaelalperovich/schooliscool/MainActivity.java
+++ app/src/main/java/com/example/michaelalperovich/schooliscool/MainActivity.java
@@ -4,9 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-import blah.Prompt;
 
 import java.util.ArrayList;
 import java.util.Random;
